07e1b5ad76ad8bc5dcc19f38f541429b7610a393
Steven Song <stsong42@gmail.com>
2024-01-25 11:27:06 -0800

    update next js configs so pictures load now

--- db/scripts/00020_multiple_photos_per_post.sql
+++ db/scripts/00020_multiple_photos_per_post.sql
@@ -9,3 +9,9 @@ CREATE TABLE photos (
 -- Remove 'photo_url' column from the 'posts' table
 ALTER TABLE posts
 DROP COLUMN photo_url;
+
+-- Insert posts
+INSERT INTO photos (photo_id, post_id, photo_url) VALUES
+  (1, 1, '1705869868856-IMG_4627.jpg'),
+  (2, 2, '1705869868859-IMG_3713.jpg'),
+  (3, 3, '1705871131326-IMG_4627.jpg');
