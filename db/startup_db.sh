7f266b4b0b021614c48269b535624f953ab4d3d4
Steven Song <stsong42@gmail.com>
2024-02-04 13:10:33 -0800

    Update startup_db.sh

--- db/startup_db.sh
+++ db/startup_db.sh
@@ -1,7 +1,4 @@
-docker kill db
-docker rm db
-docker build -t social_media_db ./db
-docker run -d -p 5432:5432 --name db social_media_db
-docker ps
-liquidbase status
+docker-compose down
+docker-compose up -d
+liquibase status
 liquibase update
